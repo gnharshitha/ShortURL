46d9b71cdca14da20a8c5b1eb315a6b41bf3f60e
Harshitha G N <gnharshitha8@gmail.com>
2025-12-17 15:00:44 +0530

    Intial commit

---
+++ urlshortener/src/main/java/com/example/urlshortener/Controller/UrlShortenerController.java
@@ -0,0 +1,14 @@
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UrlShortenerController 
+{
+    @PostMapping("/Url")
+    public String postURL(String Url){
+              
+    } 
+
+}
